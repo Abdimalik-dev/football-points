4c036b3b62dcf48250f04fb3f2c2ca66868af179
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-10-26 21:14:12 +0300

    Footbal points challenge completed

--- challenge.js
+++ challenge.js
@@ -1,2 +1,6 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function  calpoints (Wins , draw , losses) {
+    let points =  (Wins * 3) + (draw * 1) + (losses * 0) ;
+    console.log(`your Total points is ${points} `)
+}
+
+calpoints(8,3,2)
